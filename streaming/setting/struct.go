5ce51e54698c23a2e500ded46f17c2953b7d1e08
Ahri <ahri.public@gmail.com>
2026-05-08 19:51:36 +0900

    0.7.1 streaming / JSON 네이밍 변경

--- streaming/setting/struct.go
+++ streaming/setting/struct.go
@@ -3,15 +3,15 @@ package setting
 var Setting settingStruct
 
 type settingStruct struct {
-	ServerPort           string `json:"server_port"`
-	RTMPPort             string `json:"rtmp_port"`
-	HLSRoot              string `json:"hls_root"`
-	CaptureRoot          string `json:"capture_root"`
-	HLSTimeSeconds       int    `json:"hls_time_seconds"`
-	HLSListSize          int    `json:"hls_list_size"`
-	StreamTimeoutSeconds int    `json:"stream_timeout_seconds"`
-	DroneService         string `json:"drone_service"`
-	DroneValidatePath    string `json:"drone_validate_path"`
-	DroneValidateEnabled bool   `json:"drone_validate_enabled"`
-	FFmpegPath           string `json:"ffmpeg_path"`
+	ServerPort           string `json:"serverPort"`
+	RTMPPort             string `json:"rtmpPort"`
+	HLSRoot              string `json:"hlsRoot"`
+	CaptureRoot          string `json:"captureRoot"`
+	HLSTimeSeconds       int    `json:"hlsTimeSeconds"`
+	HLSListSize          int    `json:"hlsListSize"`
+	StreamTimeoutSeconds int    `json:"streamTimeoutSeconds"`
+	DroneService         string `json:"droneService"`
+	DroneValidatePath    string `json:"droneValidatePath"`
+	DroneValidateEnabled bool   `json:"droneValidateEnabled"`
+	FFmpegPath           string `json:"ffmpegPath"`
 }
