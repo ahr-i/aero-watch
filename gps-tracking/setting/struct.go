550d62063357dac0ca80a7455b30652a50552cb3
Ahri <ahri.public@gmail.com>
2026-05-08 20:49:59 +0900

    0.7.8 gps-tracking / JSON 네이밍 변경

--- gps-tracking/setting/struct.go
+++ gps-tracking/setting/struct.go
@@ -3,15 +3,15 @@ package setting
 var Setting settingStruct
 
 type settingStruct struct {
-	ServerPort                 string `json:"server_port"`
-	ServerReadHeaderTimeoutSec int    `json:"server_read_header_timeout_sec"`
-	ServerReadTimeoutSec       int    `json:"server_read_timeout_sec"`
-	ServerWriteTimeoutSec      int    `json:"server_write_timeout_sec"`
-	ServerIdleTimeoutSec       int    `json:"server_idle_timeout_sec"`
-	GPSAliveTimeoutSec         int    `json:"gps_alive_timeout_sec"`
-	GPSCleanupIntervalSec      int    `json:"gps_cleanup_interval_sec"`
-	DroneService               string `json:"drone_service"`
-	DroneValidatePath          string `json:"drone_validate_path"`
-	DroneValidateEnabled       bool   `json:"drone_validate_enabled"`
-	DroneValidateTimeoutSec    int    `json:"drone_validate_timeout_sec"`
+	ServerPort                 string `json:"serverPort"`
+	ServerReadHeaderTimeoutSec int    `json:"serverReadHeaderTimeoutSec"`
+	ServerReadTimeoutSec       int    `json:"serverReadTimeoutSec"`
+	ServerWriteTimeoutSec      int    `json:"serverWriteTimeoutSec"`
+	ServerIdleTimeoutSec       int    `json:"serverIdleTimeoutSec"`
+	GPSAliveTimeoutSec         int    `json:"gpsAliveTimeoutSec"`
+	GPSCleanupIntervalSec      int    `json:"gpsCleanupIntervalSec"`
+	DroneService               string `json:"droneService"`
+	DroneValidatePath          string `json:"droneValidatePath"`
+	DroneValidateEnabled       bool   `json:"droneValidateEnabled"`
+	DroneValidateTimeoutSec    int    `json:"droneValidateTimeoutSec"`
 }
