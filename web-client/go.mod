6306a838438cf542c6e65928159cccd986ac4b70
Ahri <ahri.public@gmail.com>
2026-05-25 19:24:58 +0900

    0.14 web-client 추가

---
+++ web-client/go.mod
@@ -0,0 +1,15 @@
+module github.com/ahr-i/aero-watch/web-client
+
+go 1.24
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/rs/cors v1.11.1
+	github.com/unrolled/render v1.7.0
+	github.com/urfave/negroni v1.0.0
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
---
+++ web-client/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/ahr-i/aero-watch/web-client/handler"
+	"github.com/ahr-i/aero-watch/web-client/setting"
+	"github.com/ahr-i/aero-watch/web-client/utils/logging"
+
+	"github.com/urfave/negroni"
+)
+
+func initiallization() {
+	logging.Init()
+	setting.Init()
+}
+
+func startServerHTTP() {
+	mux := handler.CreateHandler()
+	handler := negroni.Classic()
+	defer mux.Close()
+
+	//handler.Use(corsController.SetCors("*", "GET, POST, PUT, DELETE, OPTIONS", "*", true))
+	handler.UseHandler(mux)
+
+	logging.Info("HTTP server start.")
+	http.ListenAndServe(":"+setting.Setting.ServerPort, handler)
+}
+
+func main() {
+	initiallization()
+
+	startServerHTTP()
+}
